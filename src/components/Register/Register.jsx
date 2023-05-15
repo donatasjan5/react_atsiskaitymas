be94be9426b74cec4e297853e5e4ed7dfed0b9dc
donatasjan5 <donatasjan5@gmail.com>
2023-05-15 19:22:51 +0300

    pataisymai register formoje

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,73 +1,77 @@
-import React, { useState } from 'react';
-import './Register.css';
+import React from "react";
+import { Formik, Form, Field } from "formik";
+import "./Register.css";
 
 function Register(props) {
-  const [email, setEmail] = useState('');
-  const [pass, setPass] = useState('');
-  const [name, setName] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(email);
+  const handleSubmit = (values) => {
+    console.log(values.email);
   };
 
   return (
     <div className="container">
-      <form onSubmit={handleSubmit} className="form">
-        <label htmlFor="name" className="label">
-          Full Name
-        </label>
-        <input
-          value={name}
-          type="text"
-          name="name"
-          placeholder="Full Name"
-          className="input"
-        />
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+          confirmPassword: "",
+          fullName: "",
+        }}
+        onSubmit={handleSubmit}
+      >
+        {(formik) => (
+          <Form className="form">
+            <label htmlFor="fullName" className="label">
+              Full Name
+            </label>
+            <Field
+              type="text"
+              name="fullName"
+              placeholder="Full Name"
+              className="input"
+            />
 
-        <label htmlFor="email" className="label">
-          Email
-        </label>
-        <input
-          value={email}
-          type="email"
-          placeholder="Enter your email"
-          id="email"
-          name="email"
-          className="input"
-        />
+            <label htmlFor="email" className="label">
+              Email
+            </label>
+            <Field
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              className="input"
+            />
 
-        <label htmlFor="password" className="label">
-          Password
-        </label>
-        <input
-          value={pass}
-          type="password"
-          placeholder="*********"
-          id="password"
-          name="password"
-          className="input"
-        />
+            <label htmlFor="password" className="label">
+              Password
+            </label>
+            <Field
+              type="password"
+              name="password"
+              placeholder="*********"
+              className="input"
+            />
 
-        <label htmlFor="password" className="label">
-          Password Confirmation
-        </label>
-        <input
-          value={pass}
-          type="password"
-          placeholder="*********"
-          id="password"
-          name="password"
-          className="input"
-        />
+            <label htmlFor="confirmPassword" className="label">
+              Password Confirmation
+            </label>
+            <Field
+              type="password"
+              name="confirmPassword"
+              placeholder="*********"
+              className="input"
+            />
 
-        <button type="submit" className="button">
-          Register
-        </button>
-      </form>
+            <button type="submit" className="button">
+              Register
+            </button>
+          </Form>
+        )}
+      </Formik>
 
       <div className="button-container">
-        <button onClick={() => props.onFormSwitch('login')} className="switchButton">
+        <button
+          onClick={() => props.onFormSwitch("login")}
+          className="switchButton"
+        >
           Already have an account? Log In
         </button>
       </div>
@@ -76,3 +80,4 @@ function Register(props) {
 }
 
 export default Register;
+
